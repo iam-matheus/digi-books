9421477a33beb6663b3f78bf95883d1ac78756c2
Matheus Mulundu <61382592+iam-matheus@users.noreply.github.com>
2024-05-20 02:40:03 +0200

    update

--- js/custom.js
+++ js/custom.js
@@ -27,6 +27,33 @@
         }, 300);
       }
     });
+
+
+    // $(".anchorElement").click(function(){
+
+    //   var formdata = new FormData();
+    //   formdata.append("type", "Veetical Strip");
+    //   formdata.append("tags", "books,book club,reading,best read");
+
+    //   var ajax = new XMLHttpRequest();
+    //   ajax.addEventListener("load", completeHandler, false);
+
+    //   ajax.open("POST", "https://ad.simaneka.com/api/get");
+    //   ajax.setRequestHeader("authorisation", "i5e6Zinc3qn5loTPD1QZ6ugmOwLET8jX");
+
+    //   ajax.send(formdata);
+
+    //   function completeHandler(event) {
+    //       var response = JSON.parse(event.target.responseText);
+
+    //       console.log(response);
+    //       document.querySelector('.advertIMG').src = response.link;
+    //       document.querySelector('.advertIMG').alt = response.alt;
+    //       document.querySelector('.anchorElement').href = response.href;
+    //       document.querySelector('.headerText').innerHTML = response.message;
+    //   }
+          
+    // })
   
   })(window.jQuery);
 
